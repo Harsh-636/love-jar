fe95b514a964eab01331eabd8032ac1d55bf256b
Harsh Chaudhary <harsh.chaudhary1636@gmail.com>
2025-04-12 17:01:40 +0530

    add vite as dev dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './' // this is optional, but helpful if you have path issues
 })
